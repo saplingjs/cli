60f25e0948a851dbb4f3ca938c04cb939140c8c0
groenroos <oskari@groenroos.fi>
2020-11-16 03:32:37 +0000

    Closes #12

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,5 +1,10 @@
 import {Command, flags} from '@oclif/command'
 
+import * as path from 'path'
+import * as fs from 'fs-extra'
+import * as execa from 'execa'
+import * as getPort from 'get-port'
+
 export default class Run extends Command {
 	static description = 'Serve the Sapling project on an available port'
 
@@ -10,15 +15,26 @@ export default class Run extends Command {
 		port: flags.integer({char: 'p', description: 'Which port to use'}),
 	}
 
-	static args = []
-
 	async run() {
-		const {args, flags} = this.parse(Run)
+		const {flags} = this.parse(Run)
 
-		const name = flags.name ?? 'world'
-		this.log(`hello ${name} from /Users/groenroos/Repositories/cli/src/commands/run.ts`)
-		if (args.file && flags.force) {
-			this.log(`you input --force and --file: ${args.file}`)
+		/* Dirty check if Sapling's there */
+		if(fs.existsSync(path.join('node_modules', '@sapling', 'sapling'))) {
+			/* Get either specified port or any available port */
+			let finalPort: number;
+
+			if(!flags.port) {
+				finalPort = await getPort({port: 3000});
+			} else {
+				finalPort = flags.port;
+			}
+
+			console.log(`Running Sapling on port ${finalPort}`);
+
+			/* Execute the Sapling server with the port */
+			execa.command(`node ./node_modules/@sapling/sapling --port ${finalPort} --single`).stdout.pipe(process.stdout);
+		} else {
+			console.error('This doesn\'t seem to be a Sapling project.  Are you sure you\'re in the correct directory?');
 		}
 	}
 }
